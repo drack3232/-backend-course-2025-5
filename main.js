0d1d4d4b7a00303f44762ad23e780a5893991d58
Artem bc2025-5 <artemsenuv@gmail.com>
2025-10-28 22:14:57 +0200

    V2

--- main.js
+++ main.js
@@ -36,10 +36,78 @@ program
   }else{
     console.log(`[Info] Використовується існуюча директорія кешу: ${cacheDir}`);
   }
-  const server = http.createServer((req, res) => {
+
+function getBody(req){
+    return new Promise((resolve, reject)=> {
+        const chunks = []
+        req.on('data', chunk => chunks.push(chunk))
+        req.on('end', ()=> resolve(Buffer.concat(chunks)))
+        req.on('error', err=> reject(err))
+    })
+}
+const server = http.createServer(async (req, res)=>{
+   const { method, url } = req; 
+
+const fileName = path.basename(url);
+   if (fileName === '/' || fileName === '.' || fileName === '..')  {
     res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'})
     res.end('Сервер запущено та працює!\n');
-  })
+  }
+const filePath = path.join(cacheDir, fileName);
+
+try {
+    switch(method){
+        case 'GET':
+        try{
+            const data = await fs.readFile(filePath);
+            res.writeHead(200, {'Content-Type': 'image/jpeg'})
+            res.end(data)
+        }catch(err){
+            if(err.code ==='ENOENT'){
+                res.writeHead(404, {'Content-Type': 'text/plain; charset=utf-8'})
+                res.end('404 Not Found: Картинок нема ')
+            }else{
+                throw err;
+            }
+        }
+        break
+        case 'PUT':
+            const body = await getBody(req)
+            await fs.writeFile(filePath, body)
+            res.writeHead(201, {'Content-Type': 'text/plain; charset=utf-8'})
+            res.end('201 Created: Картинку оновлено/зБережено')
+            break;
+
+
+            case 'Delete':
+                try {
+                    await fs.unlink(filePath)
+                    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'})
+                    res.end('200 Оуйес: Картинку видалено')
+
+                } catch (err) {
+                    if (err.code ==='ENOENT'){
+                        res.writeHead(404, {'Content-Type': 'text/plain; charset=utf-8'})
+                        res.end('404 Not Found: Картинку для видалення не знайдено')
+                    }else{
+                        throw err;
+                    }
+                    
+                }
+                break;
+                default:
+        res.writeHead(405, { 'Content-Type': 'text/plain; charset=utf-8' });
+        res.end('405 Method Not Allowed');
+    }
+} catch (serverErr) {
+    console.error(`[Server Error] ${serverErr.message}`);
+    res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+    res.end('500 Internal Server Error');
+}
+
+})
+
+  
 
   server.listen(port, host, ()=> {
     console.log(`[Success] Сервер успішно запущено за адресою http://${host}:${port}`)
